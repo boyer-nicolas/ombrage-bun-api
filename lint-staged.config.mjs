d2a9a2591179ab35e80d14577f885b16857a8f47
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-11-06 11:19:07 +0100

    fix: lint staged build first to avoid phantom errors

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -6,10 +6,10 @@
 export default {
 	"*": [() => "bun run ./scripts/generate-config.ts"],
 	"**/*.ts": [
+		() => "bun run --filter ombrage-bun-api build",
 		() => "bun run lint:fix",
 		() => "bun run --filter ombrage-bun-api test:coverage",
 		() => "bun run check",
-		() => "bun run --filter ombrage-bun-api build",
 	],
 };
 
